2227bd8f6fb720c5b8327a2c3ecea181d3a12739
Nishant <palln6935@gmail.com>
2023-05-27 21:36:30 +0530

    Setup snippet serializer

--- server/settings.py
+++ server/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "snippets",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "root",
         "HOST": "localhost",
         "PORT": "5432",
     }
