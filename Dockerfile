569fec689d05d6652c7a3909e0c2027d121e5a32
prhyme <palln6935@gmail.com>
2023-08-19 19:06:02 +0530

    fix compose, definitely

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM python:3.11.4
+FROM python:3.11.4-slim-buster
 
 ENV PYTHONUNBUFFERED 1
 ENV PYTHONDONTWRITEBYTECODE 1
 
-WORKDIR /usr/src/django-rest
+WORKDIR /django-rest
 
 RUN pip install --upgrade pip
 COPY requirements.txt /tmp/
 RUN pip install --requirement /tmp/requirements.txt
 
 # copy project
-COPY . .
+# COPY . .
 
-EXPOSE 8000
+# EXPOSE 8000
 # ENTRYPOINT ["python", "manage.py"]
 # CMD ["runserver", "0.0.0.0:8000"]
--- server/settings.py
+++ server/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "foo")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -86,7 +86,7 @@ DATABASES = {
         "NAME": os.environ.get("SQL_DATABASE", "snippets"),
         "USER": os.environ.get("SQL_USER", "postgres"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "HOST": os.environ.get("SQL_HOST", "postgres"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
