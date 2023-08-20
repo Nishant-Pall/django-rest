1502b0420252f82bdcf8d6048db66c795194cf6a
prhyme <palln6935@gmail.com>
2023-08-21 01:19:19 +0530

    hyperlink entities

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,3 +1,4 @@
+from pygments import highlight
 from snippets.models import Snippet
 from rest_framework import serializers
 from django.contrib.auth.models import User
@@ -5,17 +6,30 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
 
     class Meta:
         model = Snippet
-        fields = ["id", "code", "title", "linenos", "style", "language", "owner"]
+        fields = [
+            "url",
+            "id",
+            "code",
+            "highlight",
+            "title",
+            "linenos",
+            "style",
+            "language",
+            "owner",
+        ]
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet-detail", read_only=True
     )
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
